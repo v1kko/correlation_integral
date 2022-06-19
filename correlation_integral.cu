5bd353046f72a0db979a3f6e6e9ccaad280f30bb
Victor Azizi <victor@lipsum.eu>
2022-06-19 21:31:03 +0200

    cuda implementation test

--- correlation_integral.cu
+++ correlation_integral.cu
@@ -1,24 +1,35 @@
 #include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
 #include <helper_cuda.h>
+#include <iostream>
 
-__device__ float* cd_p;
+__device__ int* cd_p;
 
-__global__ void ci_manhattan (const float *data, const float *r, int data_len, int r_len, int dims) {
+__global__ void ci_manhattan (const float *data, const float r, int data_len, int r_len, int dims) {
   int x = blockDim.x * blockIdx.x + threadIdx.x;
   int y = blockDim.y * blockIdx.y + threadIdx.y;
-  float dist = 0;
-  if (x < data_len) {
-    if (y < data_len && y > x) {
-      for (int i = 0; i < dims ; i++) {
-        dist = dist + fabsf(data[x+i] + data[y+i]);
-      }
-      for (int i = 0; i < r_len ; i++) {
-        if (r[i] > dist) {
-          cd_p[i*data_len+ x] = cd_p[i*data_len + x] + 1;
-        }
-      }
+  int b = blockIdx.x + blockIdx.y * blockDim.x;
+  int i = threadIdx.x + threadIdx.y * blockDim.x;
+  int t = blockDim.x*blockDim.y;
+  float dist = 0.f;
+  extern __shared__ int cd_reduction[];
+  cd_reduction[i] = 0;
+  if (x < data_len && y < data_len && y > x) {
+    for (int j = 0; j < dims ; j++) {
+      dist = dist + fabsf(data[x+j] + data[y+j]);
     }
+    if (r > dist) {
+      cd_reduction[i] = 1;
+    }
+  }  
+   __syncthreads();
+  for (unsigned int s = t / 2; s > 0; s >>= 1) {
+    if (i < s) {
+      cd_reduction[i] += cd_reduction[i + s];
+    }
+  }
+  if (i == 0) {
+    cd_p[b] = cd_reduction[0];
   }
 }
 
@@ -28,9 +39,7 @@ int main(void) {
   float b=0.3;
   float *x, *y;
   float *d_data;
-  float *d_r;
-  float rs[5] = {-2.5,-2,-1,-.5,0.5};
-
+  int * cd_p_h;
 
   x = (float*)malloc(15000*sizeof(float));
   y = (float*)malloc(15000*sizeof(float));
@@ -46,17 +55,24 @@ int main(void) {
   y=&y[5000];
 
 
-  checkCudaErrors(cudaMallocHost((void **)&cd_p, 5*10000*sizeof(float)));
   checkCudaErrors(cudaMallocHost((void **)&d_data, 10000*sizeof(float)));
-  checkCudaErrors(cudaMallocHost((void **)&d_r, 5*sizeof(float)));
 
   checkCudaErrors(cudaMemcpy(d_data,x,10000*sizeof(float),cudaMemcpyHostToDevice));
-  checkCudaErrors(cudaMemcpy(d_r,rs,5*sizeof(float),cudaMemcpyHostToDevice));
 
   int threadsPerBlock = 256;
   int blocksPerGrid = (10000 + threadsPerBlock - 1) / threadsPerBlock;
+
+  checkCudaErrors(cudaMallocHost((void **)&cd_p, blocksPerGrid*sizeof(int)));
+  cd_p_h =  (int *)malloc(blocksPerGrid*sizeof(int));
+
   printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid,
          threadsPerBlock);
-  ci_manhattan<<<blocksPerGrid, threadsPerBlock>>>(d_data, d_r, 10000-5, 5,5);
+  ci_manhattan<<<blocksPerGrid, threadsPerBlock,max((int)sizeof(int),(int)8)>>>(d_data, 0.5, 10000-5, 5,5);
   checkCudaErrors(cudaGetLastError());
+
+  checkCudaErrors(cudaMemcpyFromSymbol(cd_p_h,cd_p,blocksPerGrid*sizeof(int)));
+
+  for (int i = 0 ; i < blocksPerGrid ; i++) {
+    std::cout << cd_p_h[i] << " ";
+  }
 }
